2bb843bb828135e973fa4525751a8d99e7982d07
Xavier Cambar <xcambar@gmail.com>
2025-07-16 10:05:28 +0200

    feat: setup MUI with Remix properly

--- app/root.tsx
+++ app/root.tsx
@@ -1,10 +1,13 @@
+import * as React from 'react';
 import {
   Links,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
-} from "@remix-run/react";
+  useRouteError,
+  isRouteErrorResponse,
+} from '@remix-run/react';
 import type { LinksFunction } from "@remix-run/node";
 
 export const links: LinksFunction = () => [
@@ -14,16 +17,49 @@ export const links: LinksFunction = () => [
     href: "https://fonts.gstatic.com",
     crossOrigin: "anonymous",
   },
+  {
+    rel: "stylesheet",
+    href: "https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap",
+  }
 ];
+import { withEmotionCache } from '@emotion/react';
+import { unstable_useEnhancedEffect as useEnhancedEffect } from '@mui/material';
 
-export function Layout({ children }: { children: React.ReactNode }) {
+interface DocumentProps {
+  children: React.ReactNode;
+  title?: string;
+}
+
+// See https://github.com/mui/material-ui/blob/master/examples/material-ui-remix-ts/app/root.tsx
+// This is a custom Document component that wraps the application
+// It allows us to inject styles and meta tags into the HTML document
+// It also handles the Emotion cache for server-side rendering
+// This is necessary for MUI to work correctly with Remix
+const Document = withEmotionCache(({ children, title }: DocumentProps, emotionCache) => {
+
+  // Only executed on client
+  useEnhancedEffect(() => {
+    // re-link sheet container
+    emotionCache.sheet.container = document.head;
+    // re-inject tags
+    const tags = emotionCache.sheet.tags;
+    emotionCache.sheet.flush();
+    tags.forEach((tag) => {
+      // eslint-disable-next-line no-underscore-dangle
+      (emotionCache.sheet as any)._insertTag(tag);
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        {title ? <title>{title}</title> : null}
         <Meta />
         <Links />
+        <meta name="emotion-insertion-point" content="emotion-insertion-point" />
       </head>
       <body>
         {children}
@@ -32,8 +68,59 @@ export function Layout({ children }: { children: React.ReactNode }) {
       </body>
     </html>
   );
+});
+
+// https://remix.run/docs/en/main/route/component
+// https://remix.run/docs/en/main/file-conventions/routes
+export default function App() {
+  return (
+    <Document>
+        <Outlet />
+    </Document>
+  );
 }
 
-export default function App() {
-  return <Outlet />;
+// https://remix.run/docs/en/main/route/error-boundary
+export function ErrorBoundary() {
+  const error = useRouteError();
+
+  if (isRouteErrorResponse(error)) {
+    let message;
+    switch (error.status) {
+      case 401:
+        message = <p>Oops! Looks like you tried to visit a page that you do not have access to.</p>;
+        break;
+      case 404:
+        message = <p>Oops! Looks like you tried to visit a page that does not exist.</p>;
+        break;
+
+      default:
+        throw new Error(error.data || error.statusText);
+    }
+
+    return (
+      <Document title={`${error.status} ${error.statusText}`}>
+          <h1>
+            {error.status}: {error.statusText}
+          </h1>
+          {message}
+      </Document>
+    );
+  }
+
+  if (error instanceof Error) {
+    console.error(error);
+    return (
+      <Document title="Error!">
+          <div>
+            <h1>There was an error</h1>
+            <p>{error.message}</p>
+            <hr />
+            <p>Hey, developer, you should replace this with what you want your users to see.</p>
+          </div>
+      </Document>
+    );
+  }
+
+  return <h1>Unknown Error</h1>;
 }
