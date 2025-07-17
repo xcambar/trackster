3b4cbf99238a779052bb74ce619864b2c18b87a0
Xavier Cambar <xcambar@gmail.com>
2025-07-17 23:10:07 +0200

    Displays the flash messages on the main layout

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,10 @@
 import type { ActionFunctionArgs, MetaFunction } from "@remix-run/node";
 import { getEnvironment } from "../lib/environment";
+import Signin from "../components/Signin";
+import { data, useLoaderData } from "@remix-run/react";
+import { commitSession, getSession } from "~/services/session.server";
+import { AlertColor } from "@mui/material";
+import { FlashMessage } from "../components/FlashMessage";
 
 export const meta: MetaFunction = () => {
   return [
@@ -8,32 +13,43 @@ export const meta: MetaFunction = () => {
   ];
 };
 
-import Signin from "../components/Signin";
-
-
-import { useLoaderData } from "@remix-run/react";
-import { getSession } from "~/services/session.server";
-
 const checkAuthentication = async (request: Request) => {
   const session = await getSession(request.headers.get("Cookie"));
   return session.has("id") ;
 }
 
 export const loader = async ({ request }: ActionFunctionArgs) => {
-  return {
+  const session = await getSession(request.headers.get("Cookie"));
+  return data({
     features: {
       FEATURE_EMAIL_LOGIN: getEnvironment("FEATURE_EMAIL_LOGIN"),
     },
-    isLoggedIn: await checkAuthentication(request)
-  };
+    isLoggedIn: await checkAuthentication(request),
+    flash: {
+      error: session.get("error"),
+      warning: session.get("warning"),
+      info: session.get("info"),
+      success: session.get("success"),
+    },
+  }, {
+    headers: { "Set-Cookie": await commitSession(session) } 
+  });
 }
-
 
 export default function Index() {
   const loaderData = useLoaderData<typeof loader>();
   const enableEmail = loaderData.features.FEATURE_EMAIL_LOGIN === 'true';
-  if (loaderData.isLoggedIn) {
-    return <p>You are already logged in.</p>;
-  }
-  return <Signin enableEmail={enableEmail}/>;
+  return (
+    <>
+      {Object.entries(loaderData.flash).map(([severity, message]) => (
+        (message &&
+          <FlashMessage key={`${severity}-${message}`} severity={severity as AlertColor} message={message} />
+        )
+      ))}
+      {loaderData.isLoggedIn
+        ? <p>You are already logged in.</p>
+        : <Signin enableEmail={enableEmail} />
+      }
+    </>
+  );
 }
