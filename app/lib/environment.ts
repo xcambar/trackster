5918cb0ca9367b913ea56546fd120914db29b771
Xavier Cambar <xcambar@gmail.com>
2025-07-17 23:25:13 +0200

    tech: prep work for supabase

--- app/lib/environment.ts
+++ app/lib/environment.ts
@@ -6,6 +6,8 @@ export type EnvironmentVariables = {
   STRAVA_TOKEN_ENDPOINT: string;
   STRAVA_REDIRECT_URI: string;
   STRAVA_API_BASE_URL: string;
+  SUPABASE_SECRET_KEY: string;
+  SUPABASE_URL: string;
   COOKIE_SECRET: string;
   NODE_ENV: string;
 };
