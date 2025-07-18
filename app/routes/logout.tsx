45ec3076412291c16d43ef7ae70b977c963f0693
Xavier Cambar <xcambar@gmail.com>
2025-07-18 22:33:04 +0200

    feat: Started a layout, added a logout button

---
+++ app/routes/logout.tsx
@@ -0,0 +1,11 @@
+import { redirect, type ActionFunctionArgs } from "@remix-run/node";
+import { destroySession, getSession } from "~/services/session.server";
+
+export const loader = async ({ request }: ActionFunctionArgs) => {
+  const session = await getSession(request.headers.get("Cookie"));
+  return redirect("/", {
+    headers: {
+      "Set-Cookie": await destroySession(session),
+    },
+  });
+};
