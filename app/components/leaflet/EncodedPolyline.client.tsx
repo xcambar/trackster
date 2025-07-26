4582847b7acb0b6b2254a101766169ec36f72c2d
Xavier Cambar <xcambar@gmail.com>
2025-07-26 11:43:25 +0200

    tech: prepares to display encoded polylines from Strava maps

---
+++ app/components/leaflet/EncodedPolyline.client.tsx
@@ -0,0 +1,35 @@
+import {
+  createElementObject,
+  createPathComponent,
+  extendContext,
+  type PathProps,
+} from "@react-leaflet/core";
+
+import { Polyline as LeafletPolyline, type PolylineOptions } from "leaflet";
+
+import PolylineEncoded from "google-polyline";
+
+export interface PolylineProps extends PolylineOptions, PathProps {
+  encoded: string;
+}
+
+export const EncodedPolyline = createPathComponent<
+  LeafletPolyline,
+  PolylineProps
+>(
+  function createPolyline({ encoded, ...options }, ctx) {
+    const polyline = new LeafletPolyline(
+      PolylineEncoded.decode(encoded),
+      options
+    );
+    return createElementObject(
+      polyline,
+      extendContext(ctx, { overlayContainer: polyline })
+    );
+  },
+  function updatePolyline(layer, props, prevProps) {
+    if (props.encoded !== prevProps.encoded) {
+      layer.setLatLngs(PolylineEncoded.decode(props.encoded));
+    }
+  }
+);
