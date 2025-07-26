b2983e1350ab93c71bda146b8009067641eb63b8
Xavier Cambar <xcambar@gmail.com>
2025-07-26 11:46:56 +0200

    tech: refactors the creation of a Strava API client

--- app/lib/models/activity.ts
+++ app/lib/models/activity.ts
@@ -1,6 +1,7 @@
-import { ActivityType, Strava } from "strava";
+import { ActivityType } from "strava";
 import { getUserFromSession } from "./user";
-import { getEnvironment } from "lib/environment";
+import { getStravaAPIClient } from "../strava/api";
+import { User } from "@supabase/supabase-js";
 
 export type Activity = {
   type: Lowercase<ActivityType>;
@@ -15,28 +16,29 @@ type UserSession = Awaited<ReturnType<typeof getUserFromSession>>;
 export const getActivitiesForUser = async (
   user: UserSession
 ): Promise<Activity[]> => {
-  const stravaToken = user.user_metadata.strava_profile;
+  const stravaToken = user.user_metadata.strava_profile.token;
 
-  const strava = new Strava(
-    {
-      client_id: getEnvironment("STRAVA_CLIENT_ID"),
-      client_secret: getEnvironment("STRAVA_CLIENT_SECRET"),
-      on_token_refresh: (response) => {
-        /**
-         * @todo Improve on token refresh
-         */
-        console.log("REFRESH STRAVA");
-        console.log(response);
-      },
-    },
-    stravaToken.token
-  );
+  const strava = getStravaAPIClient(stravaToken);
 
   try {
     const activities = await strava.activities.getLoggedInAthleteActivities();
     return activities.map(({ id, type, start_date, name, distance }) => {
       const t = type.toLowerCase() as Lowercase<ActivityType>;
       return { id, type: t, start_date, title: name, distance };
+    });
+  } catch (error) {
+    throw new Response("Internal error", {
+      status: 500,
+    });
+  }
+};
+
+export const getActivity = async (activityId: number, user: User) => {
+  const apiClient = getStravaAPIClient(user.user_metadata.strava_profile.token);
+  try {
+    return await apiClient.activities.getActivityById({
+      id: activityId,
+      include_all_efforts: true,
     });
   } catch (error) {
     throw new Response("Internal error", {
