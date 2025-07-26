1d36f3551b383a32a2f2c1c29d9507d671521a74
Xavier Cambar <xcambar@gmail.com>
2025-07-26 11:44:51 +0200

    tech: adds a route to return a Strava activity from the Strava API
    It's a rough, experimental setup without caching or DB. It cannot go into production and DOES require refactoring

---
+++ app/routes/user.[activities.json].$id.tsx
@@ -0,0 +1,12 @@
+import { ActionFunctionArgs, data } from "@remix-run/node";
+import { getActivity } from "~/lib/models/activity";
+import { getUserFromSession } from "~/lib/models/user";
+import { ensureAuthenticatedSession } from "~/services/session.server";
+
+export const loader = async ({ request, params }: ActionFunctionArgs) => {
+  const session = await ensureAuthenticatedSession(request);
+  const user = await getUserFromSession(session);
+  const activity = await getActivity(Number(params.id), user);
+
+  return data(activity);
+};
