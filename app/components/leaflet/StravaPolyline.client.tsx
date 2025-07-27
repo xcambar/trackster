e60cc444dff7a999605a9af4aab8c07656d006e6
Xavier Cambar <xcambar@gmail.com>
2025-07-27 13:55:03 +0200

    feat: Hints at the map bychanging the color of the icon on an activity

--- app/components/leaflet/StravaPolyline.client.tsx
+++ app/components/leaflet/StravaPolyline.client.tsx
@@ -8,36 +8,10 @@ import {
 import { Polyline as LeafletPolyline, type PolylineOptions } from "leaflet";
 
 import PolylineEncoded from "google-polyline";
-import { DetailedActivity } from "strava";
-import { ActivityMap } from "~/routes/_index";
 
 interface PolylineProps extends PolylineOptions, PathProps {
   encoded: string;
 }
-
-const colors: string[] = [
-  "#FF0000",
-  "#0066FF",
-  "#00CC00",
-  "#FF6600",
-  "#9900CC",
-  "#FFCC00",
-  "#FF0099",
-  "#00CCFF",
-  "#CC3300",
-  "#006600",
-];
-
-export const buildStravaPolylineConfig = (
-  activity: DetailedActivity,
-  id: number
-): ActivityMap => {
-  return {
-    color: colors[id % colors.length] as string,
-    polyline: activity.map.polyline,
-    activityId: activity.id,
-  };
-};
 
 export const StravaPolyline = createPathComponent<
   LeafletPolyline,
