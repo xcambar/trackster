6d58b6291f8ffa1a6a9c6d149e1588b587db72e2
Xavier Cambar <xcambar@gmail.com>
2025-08-18 23:20:24 +0200

    tech: Uses pnpm for package symlinking and a better DX. Uses deno bundle to build functions. uses Deno 2 on Supabase Edge functions.

---
+++ supabase/functions/hello-world/main.test.ts
@@ -0,0 +1,10 @@
+import { assertEquals } from "@std/assert";
+import handler from "./handler.ts";
+
+Deno.test(async function serverFetch() {
+  Deno.env.set("SUPABASE_DB_URL", "ok");
+  const req = new Request("https://deno.land");
+  const res = await handler(req);
+  const body = await res.text();
+  assertEquals(body, "hello ok");
+});
