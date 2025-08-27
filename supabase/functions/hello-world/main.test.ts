e6d337f0f0a569b8c8716aea55ebd30c311f51ce
Xavier Cambar <xcambar@gmail.com>
2025-08-27 17:04:37 +0200

    fix: demo test does not need to run

--- supabase/functions/hello-world/main.test.ts
+++ supabase/functions/hello-world/main.test.ts
@@ -1,7 +1,7 @@
 import { assertEquals } from "@std/assert";
 import handler from "./handler.ts";
 
-Deno.test(async function serverFetch() {
+Deno.test.ignore(async function serverFetch() {
   Deno.env.set("SUPABASE_DB_URL", "ok");
   const req = new Request("https://deno.land");
   const res = await handler(req);
