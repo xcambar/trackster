d1ff2f9bc947135be81da2b7875afe65ea7b5f0f
Xavier Cambar <xcambar@gmail.com>
2025-09-15 18:17:43 +0000

    chore: unbuild @xcambar/env and uses the TS source directly

--- packages/@xcambar/env/dist/index.js
+++
@@ -1,6 +0,0 @@
-export function getEnvironment(k) {
-    if (!Object.keys(process.env).includes(k)) {
-        throw new Error(`Environment variable ${k} is not set`);
-    }
-    return process.env[k];
-}
