a0fa7498ad6c6f0284f9f28be2324a13a891cdd5
Xavier Cambar <xcambar@gmail.com>
2025-09-25 06:22:56 +0000

    tech: finished upgrade to ReactRouter

--- packages/@xcambar/trackster-app/vite.config.ts
+++ packages/@xcambar/trackster-app/vite.config.ts
@@ -3,24 +3,9 @@ import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-declare module "@remix-run/node" {
-  interface Future {
-    v3_singleFetch: true;
-  }
-}
-
 export default defineConfig({
   plugins: [
-    reactRouter({
-      future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-        v3_singleFetch: true,
-        v3_lazyRouteDiscovery: true,
-        v3_routeConfig: true
-      },
-    }),
+    reactRouter(),
     tsconfigPaths(),
     netlifyPlugin(),
   ],
